81ae5ccbdd910adb869f63edaf9630c4ed12c86c
Patrick Zhang <patrick8751@gmail.com>
2025-08-05 20:52:38 -0400

    project setup

---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,17 @@
+
+const HomePage = () => {
+    return (
+        <>
+            <section className="w-full h-screen bg-blue-300">
+                <h1>
+                    Curator
+                </h1>
+            </section>
+            <section className="w-full h-screen bg-red-300">
+                <h2>Hello</h2>
+            </section>
+        </>
+    )
+}
+
+export default HomePage;
