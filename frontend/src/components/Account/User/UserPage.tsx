ed32063cad9bc61d4abcbf8a0f7f49468c8d0065
Patrick Zhang <patrick8751@gmail.com>
2025-08-22 18:46:48 -0400

    Update UserPage.tsx

---
+++ frontend/src/components/Account/User/UserPage.tsx
@@ -0,0 +1,11 @@
+import UploadToolkit from "./UploadToolkit";
+
+const UserPage = () => {
+    return (
+        <div className="w-full h-screen bg-white">
+            <UploadToolkit></UploadToolkit>
+        </div>
+    )
+}
+
+export default UserPage; 
