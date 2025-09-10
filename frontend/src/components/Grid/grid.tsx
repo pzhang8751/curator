62dfb65c1c58faf359f9d5caf6b6270d377237ba
Patrick Zhang <patrick8751@gmail.com>
2025-09-10 11:34:16 -0400

    basic post display logic
    - dnd kit grid editable feature
    - modals for edit / display mode
    - modal for clicking on post

--- frontend/src/components/Grid/grid.tsx
+++ frontend/src/components/Grid/grid.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import PicturePost from "../Post/picturePost";
+import PicturePost from "../Post/picturepost";
 import { closestCenter, DndContext, type DragEndEvent } from "@dnd-kit/core";
-import GridBox from "./gridbox";
 import { arrayMove, arraySwap, SortableContext, rectSortingStrategy, rectSwappingStrategy } from "@dnd-kit/sortable";
 import { FaPencil } from "react-icons/fa6";
 
@@ -9,11 +8,7 @@ const Grid = () => {
     // need to add a list of all the components inside
     const [mode, setMode] = useState<"display" | "edit">("edit")
 
-
     const [items, setItems] = useState([1, 2, 3, 4]);
-
-
-
 
     return (
         <>
@@ -23,8 +18,9 @@ const Grid = () => {
                 } else {
                     setMode("display")
                 }
-            }}className={"absolute p-3 rounded-4xl hover:cursor-pointer " + (mode==="edit"?"bg-red-700" : "bg-green-200")}><FaPencil size={40}/></button>
-            <div className="pt-16 bg-orange-300 h-full place-self-center">
+            }} className={"z-20 absolute p-3 rounded-4xl hover:cursor-pointer " + (mode === "edit" ? "bg-red-700" : "bg-green-200")}><FaPencil size={40} /></button>
+
+            <div className="bg-orange-300 h-full w-full">
                 {mode === "edit" ? <EditMode items={items} setItems={setItems}></EditMode> : <DisplayMode items={items}></DisplayMode>}
             </div>
         </>
@@ -33,12 +29,15 @@ const Grid = () => {
 
 const DisplayMode = ({ items }: { items: Array<number> }) => {
     return (
-        <div className="grid grid-cols-2 grid-flow-dense">
+        <>
+        <div className="pt-16"></div>
+        <div className="w-[640px] grid grid-cols-4 grid-flow-dense place-self-center">
             {
                 items.map((id) =>
-                    <PicturePost key={id} ID={id} size={id}></PicturePost>)
+                    <PicturePost key={id} ID={id} size={id} edit={false}></PicturePost>)
             }
         </div>
+        </>
     )
 }
 
@@ -57,17 +56,21 @@ const EditMode = ({ items, setItems }: { items: Array<number>, setItems: React.D
     }
 
     return (
-        <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCenter}>
-            <SortableContext items={items} strategy={rectSortingStrategy}>
-                <div className="grid grid-cols-2 grid-flow-dense">
-                    {
-                        items.map((id) =>
-                            <PicturePost key={id} ID={id} size={id}></PicturePost>)
-                    }
-                </div>
+        <>
+            <div className={"w-full h-full absolute bg-black opacity-60 z-10"}></div>
+            <div className="pt-16"></div>
+            <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCenter}>
+                <SortableContext items={items} strategy={rectSortingStrategy}>
+                    <div className="w-[640px] grid grid-cols-4 grid-flow-dense relative place-self-center z-20">
+                        {
+                            items.map((id) =>
+                                <PicturePost key={id} ID={id} size={id} edit={true}></PicturePost>)
+                        }
+                    </div>
 
-            </SortableContext>
-        </DndContext>
+                </SortableContext>
+            </DndContext>
+        </>
     )
 }
 
--- frontend/src/components/Post/picturepost.tsx
+++ frontend/src/components/Post/picturepost.tsx
@@ -1,30 +1,53 @@
-import { useDraggable, type UniqueIdentifier } from "@dnd-kit/core";
+import { type UniqueIdentifier } from "@dnd-kit/core";
 import { useSortable } from "@dnd-kit/sortable";
-import {CSS} from '@dnd-kit/utilities';
+import { CSS } from '@dnd-kit/utilities';
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import { FiPlus } from "react-icons/fi";
 
-const PicturePost = ({ID, size} : {ID: UniqueIdentifier, size:number}) => {
+const PicturePost = ({ ID, size, edit }: { ID: UniqueIdentifier, size: number, edit: boolean }) => {
     //post will have a cover which can be image, or the document title - a few words? - control size option 
 
-    const {attributes, listeners, setNodeRef, transform, transition} = useSortable({id: ID});
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: ID });
 
     const style = {
-        transform: CSS.Transform.toString(transform), 
+        transform: CSS.Transform.toString(transform),
         transition
     }
 
     // can try implementing size but its gonna have to be based on fetching the data more than anything else 
 
-        // try to get just regular size working 
+    // try to get just regular size working 
     let cssSize = "w-40 h-40 col-span-1 bg-red-100";
     // if (size%3 == 0) {
     //     cssSize="w-80 h-40 col-span-2 bg-purple-300"
     // }
-    
+
+    const [open, setOpen] = useState(false)
+
     return (
-        <div ref={setNodeRef} style={style} {...listeners} {...attributes} className={"hover:cursor-grab " + cssSize}>
-            {ID}
-        </div>
+        <>
+            {!edit && open ? <Post close={() => setOpen(false)}></Post> : null}
+            <div ref={setNodeRef} style={style} {...listeners} {...attributes} className={"w-40 h-40 col-span-1 bg-red-100 " + (edit ? " hover:cursor-grab" : " hover:cursor-pointer")} onClick={!edit ? () => setOpen(true) : undefined}>
+                {ID}
+            </div>
+        </>
     )
+}
+
+
+const Post = ({close} : {close:() => void}) => {
+    return createPortal(
+        <>
+            <div className="fixed inset-0 w-full h-full bg-black opacity-60 z-30"></div>
+            <div className="top-[5%] left-[20%] absolute bg-cyan-200 h-[90%] w-[60%] z-30">
+                <button onClick={close}><FiPlus className="rotate-45 hover:cursor-pointer hover:text-red-500" size={30}/></button>
+            </div>
+        </>, document.body
+    )
+
+
+
 }
 
 export default PicturePost; 
